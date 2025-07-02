fab2e7802d887ac69d4dcb5bf48938cc74b51d00
oneezy <justinoneill2007@gmail.com>
2025-07-02 00:39:17 -0500

    style: update main page layout and enhance text styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="p-4">
+  <h1 class="text-2xl text-orange-600">Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://svelte.dev/docs/kit" class="text-gray-500">svelte.dev/docs/kit</a> to read the documentation</p>
+</main>
